65fbc16c284d73cb40105f9ab364e32315845d5a
daniil4udo <daniil4udo@gmail.com>
2022-12-06 17:45:48 +0400

    fix(types): add site/client types reference

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,4 @@
-/// <reference types="@types/imagemin-gifsicle" />
-/// <reference types="@types/imagemin-mozjpeg" />
-/// <reference types="@types/imagemin-optipng" />
-/// <reference types="@types/imagemin-svgo" />
-/// <reference types="@types/imagemin-webp" />
 /// <reference types="@types/node" />
+/// <reference types="vite/client" />
 
 export { }
