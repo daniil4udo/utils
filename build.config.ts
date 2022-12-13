5f767761694f029de7463313ad2140f9b7e1b5a3
daniil4udo <daniil4udo@gmail.com>
2022-12-13 15:56:34 +0400

    style: cleanup build file

--- build.config.ts
+++ build.config.ts
@@ -1,15 +1,15 @@
-import { defineBuildConfig } from 'unbuild'
+import { defineBuildConfig } from 'unbuild/dist/index'
 
 export default defineBuildConfig({
     name: 'utils',
     entries: [
         // default
-        './lib/index',
+        'lib/index',
         // mkdist builder transpiles file-to-file keeping original sources structure
         {
             builder: 'mkdist',
-            input: './lib/',
-            outDir: './dist/fns',
+            input: 'lib',
+            outDir: 'dist',
         },
     ],
     declaration: true,
