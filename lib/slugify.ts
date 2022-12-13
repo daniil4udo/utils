405e2dbe013a0072992da8c8daacba320758d1ae
daniil4udo <daniil4udo@gmail.com>
2022-12-13 13:47:07 +0400

    refactor(slugify): export deCamelize fn

--- lib/slugify.ts
+++ lib/slugify.ts
@@ -1,21 +1,20 @@
+export const deCamelize = (str = '') => str
+// Separate capitalized words.
+    .replace(/([A-Z]{2,})(\d+)/g, '$1 $2')
+    .replace(/([a-z\d]+)([A-Z]{2,})/g, '$1 $2')
+    .replace(/([a-z\d])([A-Z])/g, '$1 $2')
+    // `[a-rt-z]` matches all lowercase characters except `s`.
+    // This avoids matching plural acronyms like `APIs`.
+    .replace(/([A-Z]+)([A-Z][a-rt-z\d]+)/g, '$1 $2')
+
 export function slugify(string = '') {
     // Use hash map for special characters
     const specialChars = '{"à": "a","ä": "a","á": "a","â": "a","æ": "a","å": "a","ë": "e","è": "e","é": "e","ê": "e","î": "i","ï": "i","ì": "i","í": "i","ò": "o","ó": "o","ö": "o","ô": "o","ø": "o","ù": "o","ú": "u","ü": "u","û": "u","ñ": "n","ç": "c","ß": "s","ÿ": "y","œ": "o","ŕ": "r","ś": "s","ń": "n","ṕ": "p","ẃ": "w","ǵ": "g","ǹ": "n","ḿ": "m","ǘ": "u","ẍ": "x","ź": "z","ḧ": "h","·": "-","/": "-","_": "-",",": "-",":": "-",";": "-"}'
 
-    const decamelize = str => str
-    // Separate capitalized words.
-        .replace(/([A-Z]{2,})(\d+)/g, '$1 $2')
-        .replace(/([a-z\d]+)([A-Z]{2,})/g, '$1 $2')
-
-        .replace(/([a-z\d])([A-Z])/g, '$1 $2')
-    // `[a-rt-z]` matches all lowercase characters except `s`.
-    // This avoids matching plural acronyms like `APIs`.
-        .replace(/([A-Z]+)([A-Z][a-rt-z\d]+)/g, '$1 $2')
-
     if (typeof string !== 'string')
         throw new TypeError(`Expected a string, got \`${typeof string}\``)
 
-    return decamelize(string)
+    return deCamelize(string)
         .toString()
         .toLowerCase()
         .replace(/\s+/g, '-') // Replace spaces with -
