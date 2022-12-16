59ad2b99fc33c2a0d6eedcb5976dc703ddfe0de6
daniil4udo <daniil4udo@gmail.com>
2022-12-16 17:37:26 +0400

    style(isEmpty)!: rename isEmpty to isEmptyCollection

--- lib/isEmptyCollection.ts
+++ lib/isEmptyCollection.ts
@@ -1,13 +1,13 @@
 /**
  *
  * Check if collection is empty
- * Unlike Lodash throw an error on smth other the Array or Object
+ * Unlike Lodash throw an error on something other the Array or Object
  *
  * @param {Array|Object} collection
  *
  */
 import { length } from './length'
 
-export function isEmpty(collection: any): boolean {
+export function isEmptyCollection(collection: any): boolean {
     return length(collection) === 0
 }
