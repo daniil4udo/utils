4a11bb20f878e35270cca29c93a78aacb4b70085
daniil4udo <daniil4udo@gmail.com>
2022-12-30 08:05:01 +0400

    perf(filterObject): use plain filter implementation instead of native

--- lib/filterObject.ts
+++ lib/filterObject.ts
@@ -1,3 +1,22 @@
-export function filterObject<T extends object>(object: T, cb: (value: [string, any], index: number, array: [string, any][]) => unknown) {
-    return Object.fromEntries(Object.entries(object).filter(cb))
+export function filterObject<T extends object>(
+    object: T,
+    cb?: (
+        key: keyof T,
+        value: T extends ArrayLike<infer U> ? U : (T[keyof T]),
+        index: number,
+        array: T extends ArrayLike<infer U> ? [string, U][] : { [K in keyof T]: [K, T[K]] }[keyof T][]
+    ) => unknown,
+) {
+    if (typeof cb != 'function')
+        return object
+
+    const f = []
+    const entries = Object.entries(object)
+    let { length } = entries
+    while (length--) {
+        const el = entries[length]
+        if (cb(el[0], el[1], length, entries))
+            f.push(el)
+    }
+    return Object.fromEntries(f)
 }
