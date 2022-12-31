511f79e14ddb297d19121dbdbc82e5aa1194aec9
daniil4udo <daniil4udo@gmail.com>
2022-12-31 09:48:54 +0400

    refactor: schedule 1 tabindex update per animation frame

--- lib/toggleKeyboardFocus.ts
+++ lib/toggleKeyboardFocus.ts
@@ -1,17 +1,35 @@
-export const POSSIBLE_FOCUSABLE_ELS = 'a, button, input, textarea, select, details, [tabindex]:not([tabindex="-1"])' as const
+/**
+    'a[href]',
+    'area[href]',
+    'button',
+    'details',
+    'input',
+    'iframe',
+    'select',
+    'textarea',
 
-export function toggleKeyboardFocus(scope = document, tabIndex = -1) {
+    // these are actually case sensitive but i'm not listing out all the possible variants
+    '[contentEditable=""]',
+    '[contentEditable="true"]',
+    '[contentEditable="TRUE"]',
+
+    '[tabindex]:not([tabindex^="-"])',
+    ':not([disabled])';
+ */
+export const POSSIBLE_FOCUSABLE_ELS = 'a[href], area[href], button, details, input, textarea, select, [tabindex]:not([tabindex="-1"])' as const
+
+export function toggleKeyboardFocus(scope = document.body, tabIndex = -1) {
     if (!window)
         return
 
-    requestAnimationFrame(() => {
-        const elArr: HTMLElement[] = Array.from(scope.querySelectorAll(POSSIBLE_FOCUSABLE_ELS))
-        let i = elArr.length
-        const el = elArr[i]
+    const elArr: HTMLElement[] = Array.from(scope.querySelectorAll(POSSIBLE_FOCUSABLE_ELS))
+    let i = elArr.length
+    const el = elArr[i]
 
-        while (i--) {
+    while (i--) {
+        requestAnimationFrame(() => {
             if (!el.hasAttribute('disabled'))
                 el.tabIndex = tabIndex
-        }
-    })
+        })
+    }
 }
