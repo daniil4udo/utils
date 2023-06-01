a5e5d168cf1c0078f5a6ab0fe89361ad85f88ee0
daniil4udo <daniil4udo@gmail.com>
2023-06-01 17:55:44 +0400

    feature(isDefined): add new function

--- lib/hasValue.ts
+++ lib/hasValue.ts
@@ -2,6 +2,10 @@ export function isNil(val: any) {
     return val == null
 }
 
+export function isDefined(val: any) {
+    return !isNil(val)
+}
+
 export function hasValue(val: any) {
     return !isNil(val) && val !== ''
 }
