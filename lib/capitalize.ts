74de6803297416808841a1fea29821cd5c774b27
daniil4udo <daniil4udo@gmail.com>
2023-06-01 15:40:44 +0400

    feature(upper): add new upper function

--- lib/capitalize.ts
+++ lib/capitalize.ts
@@ -3,6 +3,7 @@
  * Capitalizes the first letter of a given string
  *
  * @param {string} [text=''] - The string to capitalize.
+ *
  * @returns {string} The capitalized string.
  */
 export function capitalize(text = '') {
@@ -10,4 +11,19 @@ export function capitalize(text = '') {
         return text
 
     return text.toString().charAt(0).toUpperCase() + text.slice(1)
+}
+
+/**
+ *
+ * Converts text to uppercase.
+ * @param {string} [text=''] - The text to be converted to uppercase.
+ *
+ * @returns {string} - The uppercase version of the input text.
+ *
+ * @example console.log(upper('hello')); // Output: HELLO
+*/
+export function upper(text = '') {
+    if (typeof text === 'string')
+        return text.toUpperCase()
+    return text
 }
