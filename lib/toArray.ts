11777a68f7208cd14dc30a32c6a5ff16cfbcbd3c
daniil4udo <daniil4udo@gmail.com>
2023-06-29 14:21:26 +0400

    refactor(toArray): add options argument
    Now we are able to return original array or make a copy

--- lib/toArray.ts
+++ lib/toArray.ts
@@ -1,7 +1,19 @@
-export function toArray<T>(arr: T | T[]): T[] {
+import { deepClone } from './deepClone'
+
+interface Options {
+    shallow?: boolean
+    deep?: boolean
+}
+
+export function toArray<T>(arr: T | T[], options: Options = { shallow: false, deep: false }): T[] {
     if (!arr)
         return []
-    if (Array.isArray(arr))
+    if (Array.isArray(arr)) {
+        if (options.shallow === true)
+            return [ ...arr ]
+        if (options.deep === true)
+            return deepClone(arr)
         return arr
+    }
     return [ arr ]
 }
