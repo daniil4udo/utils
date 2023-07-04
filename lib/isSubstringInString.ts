578bf57f005d1f17845fe51677c7d5aa5c3701d7
daniil4udo <daniil4udo@gmail.com>
2023-07-04 12:48:58 +0400

    refactor(isSubstringInString): stricter errors handling

--- lib/isSubstringInString.ts
+++ lib/isSubstringInString.ts
@@ -1,22 +1,32 @@
 import { toUpper } from './changeCase'
 
 /**
- * Checks if a substring is in a string.
+ * Checks if a substring exists within a string in a case-insensitive manner.
  *
- * The function converts both the input string and the substring to uppercase before checking,
+ * It converts both the input string and the substring to uppercase before checking,
  * making the check case-insensitive.
  *
- * @function isSubstringInString
- * @param {string} [string=''] - The string in which to check for the substring. Defaults to an empty string.
- * @param {string} [subString=''] - The substring to check for. Defaults to an empty string.
- * @returns {boolean} Returns `true` if the substring is found in the string, otherwise `false`.
+ * @remarks
+ * This function is part of the {@link https://github.com/daniil4udo/utils | @democrance/utils} library.
+ *
+ * @param str - The string in which to check for the substring. Defaults to an empty string.
+ * @param subString - The substring to check for. Defaults to an empty string.
+ * @returns `true` if the substring is found in the string, otherwise `false`.
+ *
+ * @throws {TypeError} If the input is not of type 'string'.
  *
  * @example
- * isSubstringInString('Hello world', 'hello') // Outputs: true
- * isSubstringInString('Hello world', 'goodbye') // Outputs: false
+ * ```ts
+ * import { isSubstringInString } from '@democrance/utils';
+ *
+ * console.log(isSubstringInString('Hello world', 'hello')); // Outputs: true
+ * console.log(isSubstringInString('Hello world', 'goodbye')); // Outputs: false
+ * ```
+ * @public
  */
-export function isSubstringInString(string = '', subString = ''): boolean {
-    return String.prototype
-        .includes
-        .call(toUpper(string), toUpper(subString))
+export function isSubstringInString(str = '', subString = '', { caseSensitive = false } = {}): boolean {
+    if (typeof str !== 'string' || typeof subString !== 'string')
+        throw new TypeError(`[isSubstringInString] - input should be type 'string'. Got ${typeof str}`)
+
+    return (caseSensitive ? str : toUpper(str)).includes(caseSensitive ? subString : toUpper(subString))
 }
--- tests/isSubstringInString.spec.js
+++ tests/isSubstringInString.spec.js
@@ -3,14 +3,41 @@ import { describe, expect, it } from 'vitest'
 import { isSubstringInString } from '../lib/isSubstringInString'
 
 describe('@/lib/isSubstringInString.ts', () => {
-    it('should match strings', () => {
-        expect(isSubstringInString('abc', 'ab')).toBe(true)
-        expect(isSubstringInString('abc', 'abcd')).toBe(false)
+    // Positive test cases
+    it('should return true when the substring exists', () => {
+        expect(isSubstringInString('Hello world', 'hello')).toBeTruthy()
+        expect(isSubstringInString('Hello world', 'WORLD')).toBeTruthy()
+        expect(isSubstringInString('123456', '234')).toBeTruthy()
     })
 
-    it('should match numbers', () => {
-        expect(isSubstringInString(1, 1)).toBe(true)
-        expect(isSubstringInString(1, 2)).toBe(false)
-        expect(isSubstringInString(12, 2)).toBe(true)
+    it('should return false when the substring does not exist', () => {
+        expect(isSubstringInString('Hello world', 'goodbye')).toBeFalsy()
+        expect(isSubstringInString('123456', '789')).toBeFalsy()
+    })
+
+    it('should return true when the substring is an empty string', () => {
+        expect(isSubstringInString('Hello world', '')).toBeTruthy()
+        expect(isSubstringInString('123456', '')).toBeTruthy()
+    })
+
+    // Negative test cases
+    it('should throw a TypeError when the inputs are not strings', () => {
+        expect(() => isSubstringInString(123, '123')).toThrow(TypeError)
+        expect(() => isSubstringInString('123', 123)).toThrow(TypeError)
+    })
+
+    it('should return true when the substring exists and caseSensitive is true', () => {
+        expect(isSubstringInString('Hello world', 'world', { caseSensitive: true })).toBeTruthy()
+        expect(isSubstringInString('123456', '234', { caseSensitive: true })).toBeTruthy()
+    })
+
+    it('should return false when the substring does not exist and caseSensitive is true', () => {
+        expect(isSubstringInString('Hello world', 'WORLD', { caseSensitive: true })).toBeFalsy()
+        expect(isSubstringInString('Hello world', 'HELLO', { caseSensitive: true })).toBeFalsy()
+        expect(isSubstringInString('123456', '2345', { caseSensitive: true })).toBeTruthy()
+    })
+
+    it('should throw a TypeError when the inputs are not strings even when caseSensitive is true', () => {
+        expect(() => isSubstringInString(123, '123', { caseSensitive: true })).toThrow(TypeError)
     })
 })
