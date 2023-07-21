2a393c09dcf83bb9f037e6c0cd51f0e13b5d04de
daniil4udo <daniil4udo@gmail.com>
2023-07-21 18:51:29 +0400

    refactor(keyBy): improve typings | throw TypeError if key cannot be an index

--- lib/keyBy.ts
+++ lib/keyBy.ts
@@ -1,17 +1,22 @@
 import { hasValue } from './hasValue'
+import { isNumber } from './isPrimitive'
+import { toType } from './toType'
 
-type Key = undefined | string | number | symbol | ((...args) => string)
-type InferredKey<T> = Key extends ((...args) => string)
-    ? string
-    : Key extends string
+type Keyable = PropertyKey | ((...args) => string) | null | undefined
+type InferredKey<T> = Keyable extends ((...args: any) => PropertyKey)
+    ? PropertyKey
+    : Keyable extends string
         ? keyof T
-        : string | number | symbol
+        : PropertyKey
 
+function isKey(input: any): boolean {
+    return typeof input === 'string' || isNumber(input) || typeof input === 'symbol'
+}
 /**
- * Creates an object composed of keys generated from the results of running each element of `a` through `k`.
- * If `k` is a function, it's invoked with one argument: the array element.
- * If `k` is a string, it's used as a property key on each element to create the returned object key.
- * If `k` is undefined or not provided, the array element itself is used as the key.
+ * Creates an object composed of keys generated from the results of running each element of `array` through `keyOrFunction`.
+ * If `keyOrFunction` is a function, it's invoked with one argument: the array element.
+ * If `keyOrFunction` is a string, it's used as a property key on each element to create the returned object key.
+ * If `keyOrFunction` is undefined or not provided, the array element itself is used as the key.
  *
  * @remarks
  * This function is a part of the {@link https://github.com/daniil4udo/utils | @democrance/utils} library.
@@ -19,8 +24,8 @@ type InferredKey<T> = Key extends ((...args) => string)
  * @template T the type of elements in the input array.
  *
  * @function keyBy
- * @param {T[]} a - The array to iterate over.
- * @param {Key} [k] - The key generation criterion. It can be a function, a string, or undefined.
+ * @param {T[]} array - The array to iterate over.
+ * @param {Keyable} [keyOrFunction] - The key generation criterion. It can be a function, a string, or undefined.
  * @returns {Record<InferredKey<T>, T>} Returns the composed aggregate object.
  *
  * @example
@@ -33,21 +38,25 @@ type InferredKey<T> = Key extends ((...args) => string)
  * ```
  * @public
  */
-export function keyBy<T>(a: T[], k?: Key) {
+export function keyBy<T>(array: T[], keyOrFunction?: Keyable) {
     const keyedCollection = {} as Record<InferredKey<T>, T>
+    const { length } = array
 
-    for (let i = 0, l = a.length; i < l; i++) {
-        const el = a[i]
+    if (hasValue(keyOrFunction) && typeof keyOrFunction !== 'function' && !isKey(keyOrFunction))
+        throw new TypeError(`[keyBy] - "${toType(keyOrFunction)}" cannot be used to index your Array`)
 
-        if (typeof k === 'function')
-            keyedCollection[k(el)] = el
+    for (let i = 0, l = length; i < l; i++) {
+        const el = array[i]
 
-        else if (typeof k === 'string')
-            keyedCollection[el[k]] = el
+        if (typeof keyOrFunction === 'function')
+            keyedCollection[keyOrFunction(el)] = el
+
+        else if (typeof keyOrFunction === 'string')
+            keyedCollection[el[keyOrFunction]] = el
 
         // If simply array if indexable values -> create object
-        else if (!hasValue(k) && (typeof el === 'string' || (typeof el === 'number' && Number(el) === el) || typeof el === 'symbol'))
-            keyedCollection[el] = el
+        else if (!hasValue(keyOrFunction) && isKey(el))
+            keyedCollection[el as PropertyKey] = el
     }
 
     return keyedCollection
