dfd7990afe5c0a7022c94c3701617c9d7e2586b0
daniil4udo <daniil4udo@gmail.com>
2023-07-21 18:51:53 +0400

    fix(length): return correct if Object has length property

--- lib/length.ts
+++ lib/length.ts
@@ -1,11 +1,19 @@
+import { isNumber } from './isPrimitive'
+import { toType } from './toType'
+
 export function length(collection: any, { includeString = false } = {}) {
     try {
         // Object.keys converts string to Array
-        return (!includeString && typeof collection === 'string')
-            ? 0
-            : collection?.length
-                ?? collection?.size
-                ?? Object.keys(collection).length
+        if ((!includeString && typeof collection === 'string'))
+            return 0
+
+        if (toType(collection) !== 'object' && isNumber(collection?.length))
+            return collection.length
+
+        if (toType(collection) !== 'object' && isNumber(collection?.size))
+            return collection.size
+
+        return Object.keys(collection).length
     }
     catch {
         return 0
