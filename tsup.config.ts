a136896255c16bb02c3cd38638fd474dfc97a672
daniil4udo <daniil4udo@gmail.com>
2023-07-26 11:11:30 +0400

    ci: fix build and release script

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,18 +2,13 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
     format: [ 'cjs', 'esm' ], // generate cjs and esm files
-    entry: [ 'lib/**/*.ts' ],
+    entry: [ 'lib/index.ts' ],
     // entryPoints: [ 'lib/index.ts' ],
     clean: true, // rimraf dis
     dts: true, // generate dts file for main module
     skipNodeModulesBundle: true,
     splitting: true,
     target: 'es2020',
-    noExternal: [
-        // 'url-template' throws require() of ES Module
-        // bundle it into the build
-        'url-template',
-    ],
     // esbuildOptions: options => {
     //     options.footer = {
     //         // This will ensure we can continue writing this plugin
